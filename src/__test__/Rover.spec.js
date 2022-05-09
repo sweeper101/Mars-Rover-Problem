85a47e12c7e696c8c47eed79d550302bb43a81c6
clifford tsungu <ctsunguc@gmail.com>
2022-05-09 12:35:11 +0100

    Spin left function test

--- src/__test__/Rover.spec.js
+++ src/__test__/Rover.spec.js
@@ -1,2 +1,8 @@
 // function to describe the possible movements for rover
-describe("Unit Movement Of Rover Tests", () => {});
+describe("Unit Movement Of Rover Tests", () => {
+  //Spinleft;
+  test("Spin Left test", () => {
+    expect(SpinLeft("N")).toBe("W");
+    expect(SpinLeft("E")).toBe("N");
+  });
+});
